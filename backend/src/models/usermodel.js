a6f90a88c786c867e68e04d8bdc4a0488e5af65c
Arjav Patel <arjav1528@gmail.com>
2025-02-16 01:18:17 +0530

    tokens

--- backend/src/models/usermodel.js
+++ backend/src/models/usermodel.js
@@ -1,49 +1,92 @@
-//create a usermodel.js file in the models folder and add the following code to it:
 
 
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const mongooseAggregatePaginate = require('mongoose-aggregate-paginate-v2');
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+    {
     
-    username : {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 2,
-        maxlength: 50
-    }, 
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true
+        username : {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength: 2,
+            maxlength: 50
+        }, 
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 6,
+            maxlength: 1024
+        },
+        profilePircture: {
+            type: String,
+            default: null
+        },
+        savedQuotes : {
+            type: Array,
+            default: [],
+        },
+        refreshToken: {
+            type: String,
+            default: null
+        },
     },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6,
-        maxlength: 1024
-    },
-    profilePircture: {
-        type: String,
-        default: null
-    },
-    savedQuotes : {
-        type: Array,
-        default: [],
-    },
-    refreshToken: {
-        type: String,
-        default: null
-    },
+    {
+        timestamps: true
+    }
 
-    
-});
+);
 
 userSchema.plugin(mongooseAggregatePaginate);
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if(user.isModified("password")){
+        user.password = await bcrypt.hash(user.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    const user = this;
+    return await bcrypt.compare(password, user.password);
+}
+
+
+userSchema.methods.generateAccessToken = function () {
+    const user = this;
+    return jwt.sign({
+        _id : user._id,
+        username: user.username
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+    )
+    
+}
+
+
+userSchema.methods.generateRefreshToken = function () {
+    const user = this;
+    return jwt.sign({
+        _id : user._id,
+        username: user.username
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    )
+     
+}
+
 
 module.exports = mongoose.model('User', userSchema);
